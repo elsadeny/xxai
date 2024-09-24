270bbbab7c8e9fbb771ef6957de494c4aa7390e3
Ishimwe Alpha <elsadeny001@gmail.com>
2024-09-24 09:00:50 +0200

    link

--- src/config-global.js
+++ src/config-global.js
@@ -7,7 +7,7 @@ import packageJson from '../package.json';
 export const CONFIG = {
   appName: 'Minimal UI',
   appVersion: packageJson.version,
-  serverUrl: import.meta.env.VITE_SERVER_URL ?? 'https://flycloud.warbller.com/',
+  serverUrl: import.meta.env.VITE_SERVER_URL ?? '',
   assetsDir: import.meta.env.VITE_ASSETS_DIR ?? '',
   /**
    * Auth
